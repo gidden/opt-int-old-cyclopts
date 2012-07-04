ec47a645ee608cb923ee107a5c7b747c945a82f5
gidden <matthew.gidden@gmail.com>
2012-07-03 20:42:50 -0500

    ok, when I went back to my laptop, it couldn't find bz2. so i'm making it a dependency, and I'm done. this process has been silly.

--- build_and_run_ex.sh
+++ build_and_run_ex.sh
@@ -1,7 +1,4 @@
-# this scripts assumes that 
-# a) you are in the cyclopts root directory
-# b) your COIN_ROOT_DIR environment variable is set
-# c) your CYCLOPTS_ROOT_DIR environment variable is set
-rm -rf build $CYCLOPTS_ROOT_DIR && mkdir build $CYCLOPTS_ROOT_DIR && cd build && cmake ../src -DCMAKE_INSTALL_PREFIX=$CYCLOPTS_ROOT_DIR && make -j3 && make install
+# this scripts assumes that you are in the cyclopts root directory
+rm -rf build  && mkdir build && cd build && cmake ../src -DCMAKE_INSTALL_PREFIX=/usr/ && make -j3 && make install
 cd ../examples
 rm -rf build && mkdir build && cd build && cmake ../src && make -j3 && ./app
